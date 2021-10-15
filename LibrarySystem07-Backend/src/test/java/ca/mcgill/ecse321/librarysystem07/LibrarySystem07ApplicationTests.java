ed8b45538d2af8e90e7a339cce3b7e53537df986
Samhandal1 <samantha.handal@mail.mcgill.ca>
2021-10-15 13:58:52 -0400

    first push

---
+++ LibrarySystem07-Backend/src/test/java/ca/mcgill/ecse321/librarysystem07/LibrarySystem07ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.librarysystem07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystem07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
