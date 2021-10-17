3cbd56de08c0d0ade97fd3c5fbbbd3a4ee99e8a8
Basel <belaghoury6@gmail.com>
2021-10-17 18:41:29 -0400

    Library Repository

--- LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/model/TimeSlot.java
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/model/TimeSlot.java
@@ -11,6 +11,24 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+//line 39 "model.ump"
+//line 152 "model.ump"
+=======
+import java.util.Map.Entry;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+//line 39 "model.ump"
+//line 152 "model.ump"
+=======
+import java.util.Map.Entry;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 // line 39 "model.ump"
 // line 152 "model.ump"
 @Entity
