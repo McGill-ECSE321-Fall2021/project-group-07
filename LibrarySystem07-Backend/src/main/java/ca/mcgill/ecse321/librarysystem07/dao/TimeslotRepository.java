5dcff1e90a7eb9f9424537c96a8745e36e331189
rebmizrahi <becca.mizrahi@gmail.com>
2021-10-17 20:54:52 -0400

    TimeSlot persistence test created

--- LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dao/TimeslotRepository.java
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dao/TimeslotRepository.java
@@ -9,7 +9,7 @@ import ca.mcgill.ecse321.librarysystem07.model.Librarian;
 import ca.mcgill.ecse321.librarysystem07.model.ReservableItem;
 
 
-public interface TimeslotRepository extends CrudRepository<TimeSlot, String>{
+public interface TimeslotRepository extends CrudRepository<TimeSlot, Integer>{
 	
 	TimeSlot findTimeSlotById(Integer id);
 
