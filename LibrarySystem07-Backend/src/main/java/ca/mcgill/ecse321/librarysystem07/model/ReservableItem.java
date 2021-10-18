8666435af103b63186598487025ea6b8681ec397
ameliebarsoum <77701656+ameliebarsoum@users.noreply.github.com>
2021-10-17 23:22:36 -0400

    Update ReservableItem.java

--- LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/model/ReservableItem.java
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/model/ReservableItem.java
@@ -1,7 +1,6 @@
-package ca.mcgill.ecse321.librarysystem07.model;
 /*PLEASE DO NOT EDIT THIS CODE*/
 /*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
-
+package ca.mcgill.ecse321.librarysystem07.model;
 
 import java.util.*;
 
@@ -44,7 +43,6 @@ public class ReservableItem extends InventoryItem
     status = aStatus;
     reservableItem = aReservableItem;
   }
-
 
   //------------------------
   // INTERFACE
