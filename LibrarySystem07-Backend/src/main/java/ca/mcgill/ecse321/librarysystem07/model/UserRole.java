925724bec3410d5192a6e8c98063ee9ea535ddca
ameliebarsoum <77701656+ameliebarsoum@users.noreply.github.com>
2021-10-17 23:30:44 -0400

    Update UserRole.java

--- LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/model/UserRole.java
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/model/UserRole.java
@@ -1,12 +1,14 @@
-package ca.mcgill.ecse321.librarysystem07.model;
 /*PLEASE DO NOT EDIT THIS CODE*/
 /*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
-
+package ca.mcgill.ecse321.librarysystem07.model;
 
 import java.util.*;
+import javax.persistence.*;
 
 // line 2 "model.ump"
 // line 97 "model.ump"
+
+@MappedSuperclass
 public abstract class UserRole
 {
 
@@ -136,6 +138,7 @@ public abstract class UserRole
     return address;
   }
 
+  @Id
   public int getLibraryCardID()
   {
     return libraryCardID;
