45a59ceb434effa79299eb861f89165a0872d835
ameliebarsoum <77701656+ameliebarsoum@users.noreply.github.com>
2021-10-17 23:11:09 -0400

    Update HeadLibrarian.java

--- LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/model/HeadLibrarian.java
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/model/HeadLibrarian.java
@@ -2,18 +2,10 @@
 /*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
 package ca.mcgill.ecse321.librarysystem07.model;
 
-
 import java.util.*;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-
+import javax.persistence.*;
 // line 23 "model.ump"
 // line 112 "model.ump"
-
 @Entity
 public class HeadLibrarian extends UserRole
 {
