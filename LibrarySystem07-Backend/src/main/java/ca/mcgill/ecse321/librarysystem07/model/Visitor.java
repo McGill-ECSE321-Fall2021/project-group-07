8e1207673b1e74acfdfb98951b5252f9b72e0c24
ameliebarsoum <77701656+ameliebarsoum@users.noreply.github.com>
2021-10-17 23:34:04 -0400

    Update Visitor.java

--- LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/model/Visitor.java
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/model/Visitor.java
@@ -1,12 +1,13 @@
-package ca.mcgill.ecse321.librarysystem07.model;
 /*PLEASE DO NOT EDIT THIS CODE*/
 /*This code was generated using the UMPLE 1.31.1.5860.78bb27cc6 modeling language!*/
-
+package ca.mcgill.ecse321.librarysystem07.model;
 
 import java.util.*;
+import javax.persistence.*;
 
 // line 11 "model.ump"
 // line 102 "model.ump"
+@Entity
 public class Visitor extends UserRole
 {
 
@@ -49,6 +50,12 @@ public class Visitor extends UserRole
   {
     return demeritPoints;
   }
+  
+ //  @Id
+//  public Integer getLibrarianCardID() {
+//	  return getLibraryCardID();
+//  }
+
   /* Code from template association_GetMany */
   public Event getEvent(int index)
   {
