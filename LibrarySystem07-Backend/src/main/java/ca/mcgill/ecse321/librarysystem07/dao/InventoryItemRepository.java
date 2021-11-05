07c2e7664d31967ea33be532571b59f67fbf7619
Samhandal1 <77597279+Samhandal1@users.noreply.github.com>
2021-11-05 12:06:54 -0400

    Add files via upload

---
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dao/InventoryItemRepository.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.librarysystem07.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.librarysystem07.model.InventoryItem;
+import ca.mcgill.ecse321.librarysystem07.model.Reservation;
+
+
+public interface InventoryItemRepository extends CrudRepository<InventoryItem, Integer> {
+	
+	InventoryItem findInventoryItemById(Integer id);
+	
+}
---
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dao/ReservationRepository.java
@@ -0,0 +1,21 @@
+package ca.mcgill.ecse321.librarysystem07.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.librarysystem07.model.*;
+
+public interface ReservationRepository extends CrudRepository<Reservation, Integer> {
+	
+	List<Reservation> findReservationsByVisitor(Visitor visitor);
+	
+	List<Reservation> findReservationByInventoryItem(InventoryItem reservableItem);
+	
+	Reservation findReservationByReservationID(Integer reservationID);
+	
+	boolean existsByInventoryItemAndVisitor(InventoryItem reservableItem, Visitor visitor);
+
+	Reservation findByInventoryItemAndVisitor(InventoryItem reservableItem, Visitor visitor);
+
+}
