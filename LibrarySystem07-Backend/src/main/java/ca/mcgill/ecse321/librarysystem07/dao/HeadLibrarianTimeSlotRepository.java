014a4358a293dc6fe77df37e52ea8a8b40027c87
Samhandal1 <77597279+Samhandal1@users.noreply.github.com>
2021-11-05 00:52:11 -0400

    Add files via upload

---
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dao/HeadLibrarianTimeSlotRepository.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse321.librarysystem07.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.librarysystem07.model.HeadLibrarian;
+import ca.mcgill.ecse321.librarysystem07.model.HeadLibrarianTimeSlot;
+
+public interface HeadLibrarianTimeSlotRepository extends CrudRepository<HeadLibrarianTimeSlot, Integer> {
+
+	List<HeadLibrarianTimeSlot> findHeadLibrarianTimeSlotByHeadLibrarian(HeadLibrarian headLibrarian);
+	HeadLibrarianTimeSlot findHeadLibrarianTimeSlotByTimeSlotID(int timeSlotId);
+}
---
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dao/LibrarianTimeSlotRepository.java
@@ -0,0 +1,15 @@
+package ca.mcgill.ecse321.librarysystem07.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.librarysystem07.model.Librarian;
+import ca.mcgill.ecse321.librarysystem07.model.LibrarianTimeSlot;
+
+public interface LibrarianTimeSlotRepository extends CrudRepository<LibrarianTimeSlot, Integer> {
+
+	List<LibrarianTimeSlot> findLibrarianTimeSlotByHeadLibrarian(Librarian Librarian);
+	LibrarianTimeSlot findLibrarianTimeSlotByTimeSlotID(int timeSlotId);
+
+}
