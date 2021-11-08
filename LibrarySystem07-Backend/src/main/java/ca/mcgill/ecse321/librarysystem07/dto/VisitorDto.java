eb407e74b7eb30e6ff123e481f84e5994bb41991
rebmizrahi <becca.mizrahi@gmail.com>
2021-11-08 18:30:17 -0500

    visitor dto made

--- LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dto/VisitorDto.java
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dto/VisitorDto.java
@@ -1,33 +1,46 @@
 package ca.mcgill.ecse321.librarysystem07.dto;
 
+import java.util.Collections;
+import java.util.List;
+
+import ca.mcgill.ecse321.librarysystem07.model.*;
+
 public class VisitorDto {
 	
+	// ATTRIBUTES
 	private float balance;
 	private int demeritPoints;
 	private String address;
 	private	String name;
 	private String username;
 	private int libraryCardId;
+	private List<Event> events;
+	private List<Reservation> reservations;
 	
+	//CONSTRUCTORS
 	public VisitorDto() {}
 	
 	public VisitorDto(String aName, String aUsername, String aAddress, int aLibraryCardID) {
-		name = aName;
-		username = aUsername;
-		address = aAddress;
-		libraryCardId = aLibraryCardID;
-		setDemeritPoints(0);
-		setBalance(0);
+		this(aName, aUsername, aAddress, aLibraryCardID, 0, Collections.EMPTY_LIST, Collections.EMPTY_LIST);
 	}
 	
+	@SuppressWarnings("unchecked")
 	public VisitorDto(String aName, String aUsername, String aAddress, int aLibraryCardID, int aDemeritPoints) {
+		this(aName, aUsername, aAddress, aLibraryCardID, aDemeritPoints, Collections.EMPTY_LIST, Collections.EMPTY_LIST);
+	}
+	
+	public VisitorDto(String aName, String aUsername, String aAddress, 
+			int aLibraryCardID, int aDemeritPoints, List<Event> events, List<Reservation> reservations) {
 		name = aName;
 		username = aUsername;
 		address = aAddress;
 		libraryCardId = aLibraryCardID;
 		setDemeritPoints(aDemeritPoints);
 		setBalance(0);
+		this.events = events;
+		this.reservations = reservations;
 	}
+	
 
 	public float getBalance() {
 		return balance;
