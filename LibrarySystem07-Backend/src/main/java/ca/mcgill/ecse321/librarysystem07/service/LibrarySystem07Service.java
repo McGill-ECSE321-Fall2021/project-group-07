b26a717071ce8d5238786cac3b4677cd21f1b7a3
rebmizrahi <becca.mizrahi@gmail.com>
2021-11-09 11:10:28 -0500

    javadocs added

--- LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/service/LibrarySystem07Service.java
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/service/LibrarySystem07Service.java
@@ -18,7 +18,7 @@ import ca.mcgill.ecse321.librarysystem07.model.InventoryItem.TypeOfItem;
 
 
 @Service
-public class LibrarySystemService {
+public class LibrarySystem07Service {
 
 	@Autowired
 	EventRepository eventRepository;
