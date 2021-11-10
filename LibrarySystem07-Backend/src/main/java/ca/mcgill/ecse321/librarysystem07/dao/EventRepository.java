f13cce9af668745179f1fb1df97f5d9e24e863c7
Samhandal1 <77597279+Samhandal1@users.noreply.github.com>
2021-11-10 01:30:38 -0500

    Add files via upload

---
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dao/EventRepository.java
@@ -0,0 +1,15 @@
+package ca.mcgill.ecse321.librarysystem07.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import ca.mcgill.ecse321.librarysystem07.model.Event;
+import ca.mcgill.ecse321.librarysystem07.model.Visitor;
+
+public interface EventRepository extends CrudRepository<Event, Integer>{
+	
+	Event findEventByEventID(Integer eventID);
+	boolean existsByVisitor(Visitor visitor);
+	List<Event> findByVisitor(Visitor visitor);
+
+}
---
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dao/VisitorRepository.java
@@ -0,0 +1,15 @@
+package ca.mcgill.ecse321.librarysystem07.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.librarysystem07.model.Event;
+import ca.mcgill.ecse321.librarysystem07.model.Reservation;
+import ca.mcgill.ecse321.librarysystem07.model.Visitor;
+
+public interface VisitorRepository extends CrudRepository<Visitor, Integer> {
+	
+	Visitor findVisitorByLibraryCardID(Integer libraryCardID);
+
+}
