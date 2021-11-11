b9d18fbc66c291e6f93ca3ca91d83c0fe905b8b2
liamopennimpede <61662350+liamopennimpede@users.noreply.github.com>
2021-11-10 21:26:45 -0500

    Update InventoryItemRepository.java

--- LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dao/InventoryItemRepository.java
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dao/InventoryItemRepository.java
@@ -3,7 +3,6 @@ package ca.mcgill.ecse321.librarysystem07.dao;
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.librarysystem07.model.InventoryItem;
-import ca.mcgill.ecse321.librarysystem07.model.Reservation;
 
 
 public interface InventoryItemRepository extends CrudRepository<InventoryItem, Integer> {
