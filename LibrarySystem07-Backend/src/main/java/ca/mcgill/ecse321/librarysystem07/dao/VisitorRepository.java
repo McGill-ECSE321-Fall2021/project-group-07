e83b9f49972019152a5973342f4cc7bfeeb54428
liamopennimpede <61662350+liamopennimpede@users.noreply.github.com>
2021-11-10 21:26:43 -0500

    Update VisitorRepository.java

--- LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dao/VisitorRepository.java
+++ LibrarySystem07-Backend/src/main/java/ca/mcgill/ecse321/librarysystem07/dao/VisitorRepository.java
@@ -1,11 +1,8 @@
 package ca.mcgill.ecse321.librarysystem07.dao;
 
-import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-import ca.mcgill.ecse321.librarysystem07.model.Event;
-import ca.mcgill.ecse321.librarysystem07.model.Reservation;
 import ca.mcgill.ecse321.librarysystem07.model.Visitor;
 
 public interface VisitorRepository extends CrudRepository<Visitor, Integer> {
